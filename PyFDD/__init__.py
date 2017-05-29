04723f712e861e148fedf47175ff907bf1ae814a
E. David-Bosne <eric.bosne@cern.ch>
2017-05-29 12:46:54 +0200

    PyFDD/__init__.py wa missing

---
+++ PyFDD/__init__.py
@@ -0,0 +1,8 @@
+"""
+PyFDD package
+"""
+
+from .fitman import fitman
+from .read2dl.lib2dl import lib2dl
+from .patterncreator import PatternCreator
+from .MedipixMatrix.MedipixMatrix import MedipixMatrix
