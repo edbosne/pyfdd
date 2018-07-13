977fc8c47b3840e85ab08515fde29ee004e14de2
E. David-Bosne <eric.bosne@cern.ch>
2018-07-13 23:09:05 +0100

    added pandas to requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='pyfdd',
-    version='0.0.01',
+    version='0.01',
     packages=['pyfdd', 'pyfdd.lib2dl', 'pyfdd.datapattern', 'pyfdd.datapattern.CustomWidgets', 'scripts', 'my_scripts',
               'test_pyfdd', 'ecsli_tools'],
     install_requires=[
-          'numpy', 'matplotlib', 'scipy', 'numdifftools']
+          'numpy', 'matplotlib', 'scipy', 'numdifftools', 'pandas'],
     url='https://github.com/eric-presbitero/pyfdd',
     license='GPL-3.0',
     author='E David-Bosne',
