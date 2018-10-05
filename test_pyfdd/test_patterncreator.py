6384dadee49262d20e666c3baac81ae858b70439
E. David-Bosne <eric.bosne@cern.ch>
2018-10-05 14:49:35 +0100

    test for pattern creator

---
+++ test_pyfdd/test_patterncreator.py
@@ -0,0 +1,28 @@
+
+
+from pyfdd import Lib2dl, PatternCreator, DataPattern, FitManager
+from pyfdd.patterncreator import create_detector_mesh
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    lib = Lib2dl("/home/eric/cernbox/University/CERN-projects/Betapix/Analysis/Channeling_analysis/FDD_libraries/GaN_89Sr/ue567g54.2dl")
+    #xmesh, ymesh = create_detector_mesh(22, 22, 1.4, 300)
+    #xmesh, ymesh = create_detector_mesh(40, 40, 0.5, 300)
+    xmesh, ymesh = create_detector_mesh(100, 100, 0.2, 300)
+    # 5 subpixels is a good number for the pads
+    gen = PatternCreator(lib, xmesh, ymesh, None, sub_pixels=5)
+
+    fractions_per_sim = np.array([1])#[0, 1])
+    #fractions_per_sim /= fractions_per_sim.sum()
+    total_events = 1
+    pattern = gen.make_pattern(0.0, -0.0, -0, fractions_per_sim, total_events, sigma=0.1, type='ideal')
+    print(pattern.sum())
+
+    plt.figure(1)
+    plt.contourf(xmesh, ymesh, pattern)
+    plt.colorbar()
+
+    plt.show()
