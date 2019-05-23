87a1e4a96806a4eed4b00a9dcc34ad4631747107
E. David-Bosne <eric.bosne@cern.ch>
2019-05-23 16:38:48 +0200

    remove prints

--- pyfdd/fitmanager.py
+++ pyfdd/fitmanager.py
@@ -694,9 +694,7 @@ class FitManager:
         else:
             sim_pattern = self._gen_detector_pattern_from_fit(fit_obj=fit_obj, generator='yield')
             total_yield = sim_pattern.sum()
-            print('total_yield', total_yield)
             #total_yield = np.sum(~self.dp_pattern.matrixCurrent.mask)
-            print('# pixels', np.sum(~self.dp_pattern.matrixCurrent.mask))
         norm_factor = None
         if normalization is None:
             norm_factor = 1
