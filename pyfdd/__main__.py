9f8ab1cf54f53301e16efcda78a77f2125894edc
E. David-Bosne <eric.bosne@cern.ch>
2020-06-30 21:06:35 +0100

    removed fbs

--- pyfdd/__main__.py
+++ pyfdd/__main__.py
@@ -1,14 +1,14 @@
 import sys
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
+#from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5 import QtCore, QtGui, QtWidgets
 from pyfdd.gui.mainwindow import WindowedPyFDD
 
 
-def run_fbs():
-    ctx = ApplicationContext()
-    window = WindowedPyFDD()
-    window.show()
-    sys.exit(ctx.app.exec_())
+#def run_fbs():
+#    ctx = ApplicationContext()
+#    window = WindowedPyFDD()
+#    window.show()
+#    sys.exit(ctx.app.exec_())
 
 
 def run():
